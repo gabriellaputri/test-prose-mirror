db534f26fd7528ce0a2c89c034b0690d0097091f
Gabriella Putri <gebipw@gmail.com>
2024-05-12 12:36:43 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div @click="init">
+      <Editor />
+    </div>
+
+    <div id="y-version"></div>
+  </div>
+</template>
+
+<script>
+import Editor from "./components/Editor";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      content: "hahah",
+      isEditor: false,
+    };
+  },
+  components: {
+    Editor,
+  },
+  methods: {
+    init(e) {
+      console.log(e.target, e.currentTarget);
+    },
+    destroy() {},
+  },
+};
+</script>
